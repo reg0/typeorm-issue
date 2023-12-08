39211f92c48df7671346a5c93f6aea4005287858
Kamil Mikolajczyk <rego@gazeta.pl>
2023-12-08 18:24:19 +0100

    added Photo entity

---
+++ src/entity/Photo.ts
@@ -0,0 +1,28 @@
+import {
+  DeleteDateColumn,
+  Entity,
+  JoinColumn,
+  OneToOne,
+  PrimaryGeneratedColumn,
+  Column,
+} from "typeorm";
+import { User } from "./User";
+
+@Entity()
+export class Photo {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @DeleteDateColumn({
+    type: "timestamp",
+    name: "deleted_at",
+  })
+  public deletedAt: Date;
+
+  @Column()
+  url: string;
+
+  @OneToOne(() => User, (user) => user.profilePicture, { nullable: false })
+  @JoinColumn({ name: "user_id" })
+  user: User;
+}
--- src/entity/User.ts
+++ src/entity/User.ts
@@ -1,18 +1,30 @@
-import { Entity, PrimaryGeneratedColumn, Column } from "typeorm"
+import {
+  Entity,
+  JoinColumn,
+  OneToOne,
+  PrimaryGeneratedColumn,
+  Column,
+} from "typeorm";
+import { Photo } from "./Photo";
 
 @Entity()
 export class User {
+  @PrimaryGeneratedColumn()
+  id: number;
 
-    @PrimaryGeneratedColumn()
-    id: number
+  @Column()
+  firstName: string;
 
-    @Column()
-    firstName: string
+  @Column()
+  lastName: string;
 
-    @Column()
-    lastName: string
+  @Column()
+  age: number;
 
-    @Column()
-    age: number
-
+  @OneToOne(() => Photo, (photo) => photo.user, {
+    nullable: true,
+    eager: true,
+  })
+  @JoinColumn({ name: "profie_picture_photo_id" })
+  profilePicture: Photo;
 }
